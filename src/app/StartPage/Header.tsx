61a2f44ca1f22e35b542175bf189b0a6eab0d91c
Filip Grafek <grafekfik737@gmail.com>
2025-02-11 10:47:48 +0100

    header changed

--- src/app/StartPage/Header.tsx
+++ src/app/StartPage/Header.tsx
@@ -26,8 +26,9 @@ const Header = () => {
   return (
     <>
     <header className='  flex fixed left-0 top-0 bg-black w-screen h-[40px]'>
-    <button className=' fixed mt-[0.25%] ml-[8%] z-0 bg-slate-500 w-[100px] rounded-md border-solid border-white p-0.5' onClick={togglePopUpSignUp}>Sign in</button>
-    <button className=' fixed mt-[0.25%] ml-[1%] z-0 bg-slate-500 w-[100px] rounded-md border-solid border-white p-0.5' onClick={togglePopUpLogIn}>Log in</button>
+      <button className=' fixed mt-[0.25%] ml-[8%] z-0 bg-slate-500 w-[100px] rounded-md border-solid border-white p-0.5' onClick={togglePopUpSignUp}>Sign in</button>
+      <button className=' fixed mt-[0.25%] ml-[1%] z-0 bg-slate-500 w-[100px] rounded-md border-solid border-white p-0.5' onClick={togglePopUpLogIn}>Log in</button>
+    
     <h1 className='text-xl ml-[50%] mt-[0.25%]'>𝓦𝓸𝓻𝓴𝓮𝓻</h1>
     </header>
     
